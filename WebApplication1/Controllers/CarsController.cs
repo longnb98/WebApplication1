ff092ca56d939097ea55ff8a4f26c686952c4ceb
longnb98 <yashagi98@gmail.com>
2021-04-27 16:58:22 +0700

    [-] Attribute Routing
    [-] Route Constraints
    
    [-] HTTPGET
    
    [-] HTTPPOST
    
    [-] HTTPPUT
    
    [-] HTTPDELETE
    
    [-] Dependency Injection
    
    [-] Swagger

---
+++ WebApplication1/Controllers/CarsController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CarsController : ControllerBase
+    {
+        IEnumerable<string> car = new List<string>
+        {
+            "Ford","Mercedes","Toyota","Kia"
+        };
+        public IEnumerable<string> Index()
+        {
+            return car;
+        }
+    }
+}
---
+++ WebApplication1/Controllers/TestController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        public Task<string> GetString()
+        {
+            return Task.FromResult<string>("test1234");
+        }
+
+        [HttpPost]
+        [Route("long")]
+        public Task<object> PostString([FromBody] object body)
+        {
+            return Task.FromResult<object>(true);
+        }
+
+        [HttpGet]
+        [Route("long")]
+        public Task<double> GetNumb()
+        {
+            return Task.FromResult<double>(95884594);
+        }
+    }
+}
